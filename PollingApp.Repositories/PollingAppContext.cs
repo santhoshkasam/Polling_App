8a6c9962a6a276cb60ef1ade93913fe10957cb3c
Parush M Taunk <ParushM.Taunk@thomsonreuters.com>
2018-12-03 15:55:12 +0530

    Files related to Category Screen

--- PollingApp.Repositories/PollingAppContext.cs
+++ PollingApp.Repositories/PollingAppContext.cs
@@ -9,32 +9,35 @@ using PollingApp.Models.Mapping;
 
 namespace PollingApp.Repositories
 {
-  public class PollingAppContext : DbContext
-  {
-    static PollingAppContext()
+    public class PollingAppContext : DbContext
     {
-      Database.SetInitializer<PollingAppContext>(null);
-    }
-    public PollingAppContext()
-      : base("name=PollingAppConnection")
-    {
-      AppRoles.AsNoTracking();
-      TopicMasters.AsNoTracking();
-      TopicOptionMappings.AsNoTracking();
-      OptionMasters.AsNoTracking();
-    }
-    public DbSet<AppRole> AppRoles { get; set; }
-    public DbSet<TopicMaster> TopicMasters { get; set; }
-    public DbSet<TopicOptionMapping> TopicOptionMappings { get; set; }
+        static PollingAppContext()
+        {
+            Database.SetInitializer<PollingAppContext>(null);
+        }
+        public PollingAppContext()
+          : base("name=PollingAppConnection")
+        {
+            AppRoles.AsNoTracking();
+            TopicMasters.AsNoTracking();
+            TopicOptionMappings.AsNoTracking();
+            OptionMasters.AsNoTracking();
+            CategoryTopicMappings.AsNoTracking();
+        }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<TopicMaster> TopicMasters { get; set; }
+        public DbSet<TopicOptionMapping> TopicOptionMappings { get; set; }
 
-    public DbSet<OptionMaster> OptionMasters { get; set; }
-    protected override void OnModelCreating(DbModelBuilder modelBuilder)
-    {
-      modelBuilder.Configurations.Add(new AppRoleMap());
-      modelBuilder.Configurations.Add(new TopicMasterMap());
-      modelBuilder.Configurations.Add(new TopicOptionMappingMap());
-      modelBuilder.Configurations.Add(new OptionMasterMap());
+        public DbSet<CategoryTopicMapping> CategoryTopicMappings { get; set; }
+        public DbSet<OptionMaster> OptionMasters { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppRoleMap());
+            modelBuilder.Configurations.Add(new TopicMasterMap());
+            modelBuilder.Configurations.Add(new TopicOptionMappingMap());
+            modelBuilder.Configurations.Add(new OptionMasterMap());
+            modelBuilder.Configurations.Add(new CategoryTopicMappingMap());
 
+        }
     }
-  }
 }
