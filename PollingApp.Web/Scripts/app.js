49062d713fea0074f450db61934eb3eec8fa33c7
Santhosh Kasam <santhosh.kasam@thomsonreuters.com>
2018-12-03 17:50:11 +0530

    Layout changes

--- PollingApp.Web/Scripts/app.js
+++ PollingApp.Web/Scripts/app.js
@@ -1,4 +1,17 @@
-﻿function ShowEntityModal(result) {
+﻿$(document).ready(function () {
+  $(".dropdown").hover(
+    function () {
+      $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+      $(this).toggleClass('open');
+    },
+    function () {
+      $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+      $(this).toggleClass('open');
+    }
+  );
+});
+
+function ShowEntityModal(result) {
   if (result) {
     if (result.responseJSON) {
       if (result.responseJSON.Status === "Failed") {
